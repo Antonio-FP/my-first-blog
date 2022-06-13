8069f6c2243ddf23aa980151beaddf68698ed188
Antonio-FP <amartinez14@alumnes.aulabalear.org>
2022-06-13 16:23:03 +0200

    Last push

--- blog/forms.py
+++ blog/forms.py
@@ -1,3 +1,4 @@
+from cProfile import label
 from django import forms
 
 from .models import Post
@@ -6,4 +7,4 @@ class PostForm(forms.ModelForm):
 
     class Meta:
         model = Post
-        fields = ('title', 'text',)
+        fields = ('title', 'text')
